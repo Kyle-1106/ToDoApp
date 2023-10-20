31f701375e36c869cce774b7aa7699c4547bb5da
Yuuki-Imagawa0827 <yuuki.imagawa0827@gmail.com>
2023-10-20 12:35:54 +0900

    エラー管理ファイル作成

---
+++ backend/services/errorMessageService.ts
@@ -0,0 +1,16 @@
+
+
+const requestInvalid="リクエストが有効ではありません"
+const invalidLoginData="ログインデータが有効ではありません"
+const failedCreateToken="トークンの作成に失敗しました"
+const failedLogin="ログインに失敗しました"
+const incorectPassword="パスワードが有効ではありません"
+
+
+module.exports={
+  requestInvalid,
+  invalidLoginData,
+  failedCreateToken,
+  failedLogin,
+  incorectPassword,
+}
